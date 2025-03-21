a83c82f09a71222cd131fb260ab0c22be28f8cd8
hiepdv <hiep.dv@zinza.com.vn>
2025-03-21 18:05:07 +0700

    login page

---
+++ frontend/src/pages/auth/Login.js
@@ -0,0 +1,61 @@
+import React from "react";
+import "../../public/auth/login.css"; // Đảm bảo CSS nằm trong thư mục src
+import videoBg from "../../public/auth/Ipad.mp4";
+const Login = () => {
+  return (
+    <div className="video-container">
+      <video autoPlay loop muted className="background-video">
+        <source src={videoBg} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+      <div className="content">
+        <div className="main-content-agile">
+          <div className="sub-main-w3">
+            <div className="wthree-pro">
+              <h2>Login Here</h2>
+            </div>
+            <form action="#" method="post">
+              <div className="input-container">
+                <input
+                  placeholder="Username or E-mail"
+                  name="Name"
+                  className="user"
+                  type="email"
+                  required
+                />
+                <span className="icon1">
+                  <i className="fa fa-user" aria-hidden="true"></i>
+                </span>
+              </div>
+
+              <div className="input-container">
+                <input
+                  placeholder="Password"
+                  name="Password"
+                  className="pass"
+                  type="password"
+                  required
+                />
+                <span className="icon2">
+                  <i className="fa fa-unlock" aria-hidden="true"></i>
+                </span>
+              </div>
+
+              <div className="sub-w3l">
+                <h6>
+                  <a href="#">Forgot Password?</a>
+                </h6>
+                <div className="right-w3l">
+                  <input type="submit" value="Login" />
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
